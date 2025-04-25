4d94b5b5871b1f7bf168e0cdfb87d61c04405482
Shakir Chhawniwala <mshakir746@gmail.com>
2025-04-25 14:26:54 +0530

    structuring project

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// filepath: /Users/moments/Desktop/Shakir Chhawniwala/Programming/Fullstack/Dairy Farm/daily-dairy/src/components/Header.jsx
+function Header() {
+    return (
+      <header>
+        <h1>Daily Dairy Management</h1>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/animals">Animals</a>
+          <a href="/reports">Reports</a>
+        </nav>
+      </header>
+    )
+  }
+  
+  export default Header
